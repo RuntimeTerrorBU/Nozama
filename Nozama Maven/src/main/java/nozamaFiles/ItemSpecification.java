3dc4e3876e5581e029d2bb3bd8a9988ec10d1433
TylerRoss212 <Tyler_Ross3@Baylor.edu>
2021-03-28 18:43:40 -0500

    Changed product to item and fixed the classes

--- Nozama Maven/src/main/java/nozamaFiles/ItemSpecification.java
+++ Nozama Maven/src/main/java/nozamaFiles/ItemSpecification.java
@@ -1,34 +1,51 @@
 package nozamaFiles;
 
-public class ProductSpecification {
+public class ItemSpecification {
 	private String description;
 	private Double price;
 	private String itemID;
+	private Integer quantity;
 	
-	public ProductSpecification(String description, Double price, String itemID) {
+	public ItemSpecification(String description, Double price, String itemID, Integer quantity) {
 		super();
 		this.description = description;
 		this.price = price;
 		this.itemID = itemID;
+		this.quantity = quantity;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 	public String getItemID() {
 		return itemID;
 	}
+
 	public void setItemID(String itemID) {
 		this.itemID = itemID;
 	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -36,8 +53,10 @@ public class ProductSpecification {
 		result = prime * result + ((description == null) ? 0 : description.hashCode());
 		result = prime * result + ((itemID == null) ? 0 : itemID.hashCode());
 		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -46,7 +65,7 @@ public class ProductSpecification {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		ProductSpecification other = (ProductSpecification) obj;
+		ItemSpecification other = (ItemSpecification) obj;
 		if (description == null) {
 			if (other.description != null)
 				return false;
@@ -61,6 +80,11 @@ public class ProductSpecification {
 			if (other.price != null)
 				return false;
 		} else if (!price.equals(other.price))
+			return false;
+		if (quantity == null) {
+			if (other.quantity != null)
+				return false;
+		} else if (!quantity.equals(other.quantity))
 			return false;
 		return true;
 	}
