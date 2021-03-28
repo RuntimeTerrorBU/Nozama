bce2963a72593a4606b80f43978f8f6dd2b36674
Alehman74 <Alehman74@yahoo.com>
2021-03-28 17:16:02 -0500

    Added Item.java

---
+++ Nozama Maven/src/main/java/nozamaFiles/Item.java
@@ -0,0 +1,76 @@
+package nozamaFiles;
+
+public class Item {
+	private String name;
+	private Double cost;
+	private int serialNumber;
+	private int numAvailable;
+	
+	public Item(String name, Double cost, int serialNumber, int numAvailable) {
+		super();
+		this.name = name;
+		this.cost = cost;
+		this.serialNumber = serialNumber;
+		this.numAvailable = numAvailable;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cost == null) ? 0 : cost.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + numAvailable;
+		result = prime * result + serialNumber;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (cost == null) {
+			if (other.cost != null)
+				return false;
+		} else if (!cost.equals(other.cost))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (numAvailable != other.numAvailable)
+			return false;
+		if (serialNumber != other.serialNumber)
+			return false;
+		return true;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getCost() {
+		return cost;
+	}
+	public void setCost(Double cost) {
+		this.cost = cost;
+	}
+	public int getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(int serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	public int getNumAvailable() {
+		return numAvailable;
+	}
+	public void setNumAvailable(int numAvailable) {
+		this.numAvailable = numAvailable;
+	}
+	
+}
