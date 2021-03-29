30ec17e3b1ec38ebdc7540e41f75b5ade5b467f9
Alehman74 <Alehman74@yahoo.com>
2021-03-29 14:29:28 -0500

    Updated Item.java

--- Nozama Maven/src/main/java/nozamaFiles/Item.java
+++ Nozama Maven/src/main/java/nozamaFiles/Item.java
@@ -3,28 +3,29 @@ package nozamaFiles;
 import nozamaFiles.Item;
 
 public class Item {
-	private String name;
-	private Double cost;
-	private int serialNumber;
-	private int numAvailable;
-	
-	public Item(String name, Double cost, int serialNumber, int numAvailable) {
+	private String itemID;
+
+	public Item(String itemID) {
 		super();
-		this.name = name;
-		this.cost = cost;
-		this.serialNumber = serialNumber;
-		this.numAvailable = numAvailable;
+		this.itemID = itemID;
 	}
+
+	public String getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(String itemID) {
+		this.itemID = itemID;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((cost == null) ? 0 : cost.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + numAvailable;
-		result = prime * result + serialNumber;
+		result = prime * result + ((itemID == null) ? 0 : itemID.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -34,45 +35,11 @@ public class Item {
 		if (getClass() != obj.getClass())
 			return false;
 		Item other = (Item) obj;
-		if (cost == null) {
-			if (other.cost != null)
+		if (itemID == null) {
+			if (other.itemID != null)
 				return false;
-		} else if (!cost.equals(other.cost))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (numAvailable != other.numAvailable)
-			return false;
-		if (serialNumber != other.serialNumber)
+		} else if (!itemID.equals(other.itemID))
 			return false;
 		return true;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Double getCost() {
-		return cost;
-	}
-	public void setCost(Double cost) {
-		this.cost = cost;
-	}
-	public int getSerialNumber() {
-		return serialNumber;
-	}
-	public void setSerialNumber(int serialNumber) {
-		this.serialNumber = serialNumber;
-	}
-	public int getNumAvailable() {
-		return numAvailable;
-	}
-	public void setNumAvailable(int numAvailable) {
-		this.numAvailable = numAvailable;
-	}
-	
 }
