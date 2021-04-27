0093b1fdf4aaf9185f0b28f309a792b8a8039c15
JoshuaHunter16 <jchunter816@gmail.com>
2021-04-27 05:21:54 -0500

    Shopping Cart and Wishlist View Overhaul

--- Nozama Maven/src/main/java/views/ShoppingCartView.java
+++ Nozama Maven/src/main/java/views/ShoppingCartView.java
@@ -17,46 +17,46 @@ import javax.swing.*;
 import controllers.*;
 
 public class ShoppingCartView extends JPanel implements ActionListener {
-	
-	//Instance of the ShoppingCart Model to be used
+
+	// Instance of the ShoppingCart Model to be used
 	private static ShoppingCartController scm = new ShoppingCartController();
 
 	public ShoppingCartView(Customer c) {
+
 		// FIXME This try/catch is specifically for testing only
 		try {
+			// Initialize the files
 			File catalogFile = new File("resources/testCatalog.csv");
 			ItemCatalog.loadData(catalogFile);
-			//scm.setCart(sc);
 			scm.setCustomer(c);
 			scm.setCustomerFile(new File("resources/carts/" + c.getUsername() + "Cart.csv"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		//Creation of Table to add initial interface instance to
+		// Creation of Table to add initial interface instance to
 		final JTable table = new JTable(scm);
 		table.removeColumn(table.getColumnModel().getColumn(4));
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane);
-		
-		//Initialization of labels used
+
+		// Initialization of labels used
 		JLabel subtotalLbl = new JLabel();
-		
-		//Fetch the sub total of the current cart, used to display
+
+		// Fetch the sub total of the current cart, used to display
 		BigDecimal sub = new BigDecimal(c.getCustomerCart().calculateSub()).setScale(2, RoundingMode.HALF_UP);
 		subtotalLbl.setText("Subtotal: $" + sub.toPlainString());
 
-		//Action to perform edit on quantity of item
+		// Action to perform edit on quantity of item
 		Action edit = new AbstractAction() {
 			public void actionPerformed(ActionEvent e) {
-				
-				//Creation of edit prompt panel
+
+				// Creation of edit prompt panel
 				JPanel form2 = new JPanel(new GridLayout(0, 1));
 				JTable table = (JTable) e.getSource();
 				int modelRow = Integer.valueOf(e.getActionCommand());
 
-				//Text label to show  user prompt and input field
+				// Text label to show user prompt and input field
 				JTextField numField = new JTextField(table.getModel().getValueAt(modelRow, 2).toString());
 				numField.setSize(new Dimension(75, 30));
 				JLabel numLabel = new JLabel("Enter Quantity: ");
@@ -64,32 +64,32 @@ public class ShoppingCartView extends JPanel implements ActionListener {
 				numLabel.setLabelFor(numField);
 				form2.add(numField);
 
-				//Change the initial UI to reflect options for Save/Cancel
+				// Change the initial UI to reflect options for Save/Cancel
 				UIManager.put("OptionPane.cancelButtonText", "Cancel");
 				UIManager.put("OptionPane.okButtonText", "Save");
-				
-				//Set result based on input option
+
+				// Set result based on input option
 				int result = JOptionPane.showConfirmDialog(null, form2, "Edit Quantity", JOptionPane.OK_CANCEL_OPTION);
 
-				//If the option is to Save
+				// If the option is to Save
 				if (result == JOptionPane.OK_OPTION) {
-					
-					//If quantity changed to 0, remove item
+
+					// If quantity changed to 0, remove item
 					int res = Integer.parseInt(numField.getText());
 					if (res == 0) {
 						((ShoppingCartController) table.getModel()).removeRow(modelRow);
 
-					} 
-					//ERROR if negative quantity
-					else if(res < 0) {
-						JOptionPane.showMessageDialog(form2, "ERROR: You cannot enter a negative quantity!");
-						
 					}
-					//Change quantity if valid quantity & write to the customer file
+					// ERROR if negative quantity
+					else if (res < 0) {
+						JOptionPane.showMessageDialog(form2, "ERROR: You cannot enter a negative quantity!");
+
+					}
+					// Change quantity if valid quantity & write to the customer file
 					else if (res <= ItemCatalog.getItemSpecification(scm.getValueAt(modelRow, 4).toString())
 							.getQuantity()) {
 						scm.setValueAt(res, modelRow, 2);
-						
+
 						try {
 							BufferedWriter out = new BufferedWriter(new FileWriter(scm.getCustomerFile()));
 							out.write(scm.getCart().toString());
@@ -98,190 +98,198 @@ public class ShoppingCartView extends JPanel implements ActionListener {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
-						
+
 					}
-					//Otherwise quantity is too large for order, ERROR
+					// Otherwise quantity is too large for order, ERROR
 					else {
 						JOptionPane.showMessageDialog(form2, "ERROR: Quantity entered exceeded maximum availiable!");
 					}
-					
-					//Edit sub total to reflect changes
+
+					// Edit sub total to reflect changes
 					BigDecimal sub = new BigDecimal(scm.getSubtotal()).setScale(2, RoundingMode.HALF_UP);
 					subtotalLbl.setText("Subtotal: $" + sub.toPlainString());
-					
-					//Fire changes (safety)
+
+					// Fire changes (safety)
 					revalidate();
 				}
 			}
 		};
-		
-		//Creation of edit button column (for each item)
+
+		// Creation of edit button column (for each item)
 		ButtonColumn editButton = new ButtonColumn(table, edit, 3);
 
-		//Add the sub total, or updated sub total interface display
+		// Add the sub total, or updated sub total interface display
 		add(subtotalLbl, BorderLayout.SOUTH);
 
-		//Creation of checkout button
+		// Creation of checkout button
 		JButton checkoutButton = new JButton("Checkout");
 		checkoutButton.addActionListener(this);
 		add(checkoutButton);
-		
-		//Fire changes
+
+		// Fire changes
 		revalidate();
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		JButton source = (JButton) (e.getSource());
 
-		//If the action fired is the checkout confirmation
+		// If the action fired is the checkout confirmation
 		if (source.getText().equals("Checkout")) {
-			//Base of Checkout Confirmation
-			
-			//Frame and label creation
+			// Base of Checkout Confirmation
+
+			// Frame and label creation
 			JPanel checkoutForm = new JPanel(new GridLayout(0, 1));
 			JLabel subtotalLbl = new JLabel();
-			
-			//Frame altered to be configured as a checkout screen
-			/*frameCheckout.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			frameCheckout.setPreferredSize(new Dimension(240, 150));
-			frameCheckout.pack();
-			frameCheckout.setLocationRelativeTo(null);
-			frameCheckout.setVisible(true);*/
-			
-			//Fire changes
+
+			// Fire changes
 			revalidate();
-			
-			//Find the sub total based on the Shopping Cart Model		
+
+			// Find the sub total based on the Shopping Cart Model
 			BigDecimal sub = new BigDecimal(scm.getSubtotal()).setScale(2, RoundingMode.HALF_UP);
 			subtotalLbl.setText('\t' + "Subtotal: $" + sub.toPlainString());
 			subtotalLbl.setVisible(true);
-			//JPanel checkoutForm = new JPanel(new GridLayout(0, 1));
-			//add(checkoutForm);
-			//setVisible(checkoutForm);	
-			
-			//Create Labels and Text fields for entering card info and cvc numbers
+
+			// Create Labels / text field for address
 			JLabel addyLabel = new JLabel("Enter Address: ");
 			JTextField addyField = new JTextField();
 			addyField.setSize(new Dimension(75, 30));
+
+			// Create Labels / text field for card
 			JLabel cardLabel = new JLabel("Enter Card Number: ");
 			JTextField cardField = new JTextField();
 			cardField.setSize(new Dimension(75, 30));
+
+			// Create Labels / text field for cvc
 			JLabel cvcLabel = new JLabel("Enter CVC Number: ");
 			JTextField cvcField = new JTextField();
 			cvcField.setSize(new Dimension(75, 30));
-			
-			//Add the sub total count to the checkout prompt
+
+			// Add the sub total count to the checkout prompt
 			subtotalLbl.setHorizontalAlignment(JLabel.CENTER);
 			checkoutForm.add(subtotalLbl, BorderLayout.NORTH);
-			
-			//Edit layout for address prompt
+
+			// Edit layout for address prompt
 			checkoutForm.add(addyLabel, BorderLayout.WEST);
 			addyLabel.setLabelFor(addyField);
 			checkoutForm.add(addyField);
-			
-			//Edit layout for card input prompt
+
+			// Edit layout for card input prompt
 			checkoutForm.add(cardLabel, BorderLayout.WEST);
 			cardLabel.setLabelFor(cardField);
 			checkoutForm.add(cardField);
-			
-			//Edit layout for cvc input prompt
+
+			// Edit layout for cvc input prompt
 			checkoutForm.add(cvcLabel, BorderLayout.WEST);
 			cvcLabel.setLabelFor(cvcField);
 			checkoutForm.add(cvcField);
-			
-			//Add the entire form to the frame
-			//frameCheckout.add(checkoutForm);
-			
-			//Fire changes
+
+			// Fire changes
 			revalidate();
-			
+
+			// Initialize button text
 			UIManager.put("OptionPane.cancelButtonText", "Cancel");
 			UIManager.put("OptionPane.okButtonText", "Checkout");
+
+			// Get the result of the button press to check if continue
 			int res = JOptionPane.showConfirmDialog(null, checkoutForm, "Checkout", JOptionPane.OK_CANCEL_OPTION);
-			
-			if(res == JOptionPane.OK_OPTION) {
+
+			// If OK pressed, continue
+			if (res == JOptionPane.OK_OPTION) {
+
+				// Initialize the JPanel for checkout
 				JPanel frameCheckout = new JPanel();
-				
-				//Frame altered to be configured as a checkout screen
-				//frameCheckout.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-				frameCheckout.setPreferredSize(new Dimension(240, 150));
-				//frameCheckout.pack();
-				//frameCheckout.setLocationRelativeTo(null);
-				frameCheckout.setVisible(true);
-				
+
+				// Initialize the receipt labels
 				JLabel thankyou = new JLabel("Thank you for your purchase!");
 				JLabel delimeter = new JLabel("===========================");
 				JLabel header = new JLabel("Shopping Purchase Details: ");
 				JLabel totalPay = new JLabel("Total Purchase Cost: " + sub.toPlainString());
 				JLabel sentAddress = new JLabel("Address: " + addyField.getText());
 				JLabel cardUsed = new JLabel("Card: " + cardField.getText());
-				
+
+				// Edit the Frame settings
+				frameCheckout.setPreferredSize(new Dimension(240, 150));
+				frameCheckout.setVisible(true);
+
+				// Add the labels to the checkout frame
 				frameCheckout.add(thankyou, BorderLayout.WEST);
 				frameCheckout.add(delimeter, BorderLayout.WEST);
 				frameCheckout.add(header, BorderLayout.WEST);
 				frameCheckout.add(totalPay, BorderLayout.WEST);
 				frameCheckout.add(sentAddress, BorderLayout.WEST);
 				frameCheckout.add(cardUsed, BorderLayout.WEST);
-				
+
+				// Fire changes
 				frameCheckout.revalidate();
-				
-				// record purchase
+
+				// record purchase & initialize files
 				File file = new File("resources/orders/" + scm.getCustomer().getUsername() + "Orders.csv");
 				File reportFile = new File("resources/orders/GeneratedReport.csv");
 				FileWriter fwriter;
 				FileWriter reportWriter;
+
 				try {
+					// Set readers if valid
 					fwriter = new FileWriter(file, true);
 					reportWriter = new FileWriter(reportFile, true);
-					
+
+					// Initialize buffered readers for file writing
 					BufferedWriter writer = new BufferedWriter(fwriter);
 					BufferedWriter rWriter = new BufferedWriter(reportWriter);
-					
 					writer.write("\n");
+
+					// Send the output to the output files for generation
 					for (Pair<Item, Integer> p : scm.getCustomer().getCustomerCart().getCart()) {
 						writer.write(p.first.getItemID() + "," + p.second + "\n");
-						rWriter.write(p.first.getItemID()+ "," + p.second + "\n");
+						rWriter.write(p.first.getItemID() + "," + p.second + "\n");
 					}
-					
+
+					// Close the buffers
 					writer.close();
 					rWriter.close();
+
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
-				
-				// show shipping confirmation
+
+				// Initialize J-Style Objects for shipping confirmation
 				JFrame cFrame = new JFrame("Shipping confirmation");
 				JPanel cPanel = new JPanel(new GridLayout(0, 1));
-				
 				JLabel title = new JLabel("Shipping detals:");
 				JTextField itemsField = new JTextField();
 				String itemsString = "";
-				
+
+				// Add the title and address to the panel
 				cPanel.add(title);
 				cPanel.add(sentAddress);
-				
+
+				// Add the individual item fields
 				for (Pair<Item, Integer> p : scm.getCustomer().getCustomerCart().getCart()) {
 					itemsField = new JTextField();
-					itemsString = "Item: " + ItemCatalog.getItemSpecification(p.first.getItemID()).getName() +
-							" Quantity: " + p.second + "\n";
+					itemsString = "Item: " + ItemCatalog.getItemSpecification(p.first.getItemID()).getName()
+							+ " Quantity: " + p.second + "\n";
 					itemsField.setText(itemsString);
 					itemsField.setEditable(false);
 					cPanel.add(itemsField);
 				}
-				
-				// display it
+
+				// Set frame settings and display
 				cFrame.getContentPane().add(cPanel);
 				cFrame.pack();
 				cFrame.setVisible(true);
-				
+
+				// Set the button names for receipt prompting
 				UIManager.put("OptionPane.okButtonText", "Receipt");
 				UIManager.put("OptionPane.cancelButtonText", "No Receipt");
-				
-				int resRec = JOptionPane.showConfirmDialog(null, frameCheckout, "Purchase Confirmation!", JOptionPane.OK_CANCEL_OPTION);
-				
-				
-				if(resRec == JOptionPane.OK_OPTION) {
-					
+
+				// Determine if receipt is wanted
+				int resRec = JOptionPane.showConfirmDialog(null, frameCheckout, "Purchase Confirmation!",
+						JOptionPane.OK_CANCEL_OPTION);
+
+				// If receipt needs to be generated
+				if (resRec == JOptionPane.OK_OPTION) {
+
+					// Initialize receipt generation variables
 					JPanel exportPrompt = new JPanel();
 					int selectedOpt;
 
@@ -293,34 +301,38 @@ public class ShoppingCartView extends JPanel implements ActionListener {
 					// Chunk to write to file and export
 					if (optChosen == JFileChooser.APPROVE_OPTION) {
 						try {
+
+							// Initialize files
 							File expFile = fileChosen.getSelectedFile();
 							FileWriter toWrite = new FileWriter(expFile);
 
 							// Call Model function to return table that is formatted for export
-							toWrite.write("Thank you for your purchase!\n===========================\nPurchase Details:\n" +
-											"Total: " + sub.toPlainString() + 
-											"\nAddress: " + addyField.getText() +
-											"\nCard: " + cardField.getText());
+							toWrite.write("Thank you for your purchase!\n===========================\nPurchase Details:\n"
+											+ "Total: " + sub.toPlainString() + "\nAddress: " + addyField.getText()
+											+ "\nCard: " + cardField.getText());
 							toWrite.close();
 						} catch (IOException x) {
 							x.printStackTrace();
 						}
 					}
-					
+
 				}
 			}
 		}
 	}
 
 	public static void createAndShowGUI(Customer c) {
-		//Implementation of the Shopping cart interface after checkout
+
+		// Implementation of the Shopping cart interface after checkout
+		// Initialize the frame and set initial setting
 		JFrame frame = new JFrame("Your Cart");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+		// Initialize the initial shopping char view
 		ShoppingCartView newContentPane = new ShoppingCartView(c);
-
 		newContentPane.setOpaque(true);
 
+		// Set frame settings
 		frame.setContentPane(newContentPane);
 		frame.pack();
 		frame.setVisible(true);
--- Nozama Maven/src/main/java/views/WishlistView.java
+++ Nozama Maven/src/main/java/views/WishlistView.java
@@ -28,41 +28,43 @@ import nozamaFiles.ItemCatalog;
 import controllers.WishlistController;
 
 public class WishlistView extends JPanel {
-	//Instance of the ShoppingCart Model to be used
+
+	// Instance of the Wish list Controller to be used
 	private static WishlistController wc = new WishlistController();
 
-	public WishlistView(Customer c) {	
+	public WishlistView(Customer c) {
 		try {
+			// Set up files based on the set catalog
 			File catalogFile = new File("resources/testCatalog.csv");
 			ItemCatalog.loadData(catalogFile);
 			wc.setCustomer(c);
 			File w = new File("resources/wishlists/" + c.getUsername() + "Wishlist.csv");
 			wc.setCustomerFile(w);
-			
 			BufferedWriter out = new BufferedWriter(new FileWriter(wc.getCustomerFile()));
-			//System.out.println(wc.getCart().toString());
+
+			// Write the cart to the out
 			out.write(wc.getCart().toString());
 			out.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//Creation of Table to add initial interface instance to
+
+		// Creation of Table to add interface instance to
 		final JTable table = new JTable(wc);
 		table.removeColumn(table.getColumnModel().getColumn(4));
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane);
 
-		//Action to perform edit on quantity of item
+		// Action to perform edit on quantity of item
 		Action edit = new AbstractAction() {
 			public void actionPerformed(ActionEvent e) {
-				
-				//Creation of edit prompt panel
+
+				// Creation of edit prompt panel
 				JPanel form2 = new JPanel(new GridLayout(0, 1));
 				JTable table = (JTable) e.getSource();
 				int modelRow = Integer.valueOf(e.getActionCommand());
 
-				//Text label to show user prompt and input field
+				// Text label to show user prompt and input field
 				JTextField numField = new JTextField(table.getModel().getValueAt(modelRow, 2).toString());
 				numField.setSize(new Dimension(75, 30));
 				JLabel numLabel = new JLabel("Enter Quantity: ");
@@ -70,71 +72,73 @@ public class WishlistView extends JPanel {
 				numLabel.setLabelFor(numField);
 				form2.add(numField);
 
-				//Change the initial UI to reflect options for Save/Cancel
+				// Change the initial UI to reflect options for Save/Cancel
 				UIManager.put("OptionPane.cancelButtonText", "Cancel");
 				UIManager.put("OptionPane.okButtonText", "Save");
-				
-				//Set result based on input option
+
+				// Set result based on input option
 				int result = JOptionPane.showConfirmDialog(null, form2, "Edit Quantity", JOptionPane.OK_CANCEL_OPTION);
 
-				//If the option is to Save
+				// If the option is to Save
 				if (result == JOptionPane.OK_OPTION) {
-					
-					//If quantity changed to 0, remove item
+
+					// If quantity changed to 0, remove item
 					int res = Integer.parseInt(numField.getText());
 					if (res == 0) {
 						((WishlistController) table.getModel()).removeRow(modelRow);
 
-					} 
-					//ERROR if negative quantity
-					else if(res < 0) {
-						JOptionPane.showMessageDialog(form2, "ERROR: You cannot enter a negative quantity!");
-						
 					}
-					//Change quantity if valid quantity & write to the customer file
+					// ERROR if negative quantity
+					else if (res < 0) {
+						JOptionPane.showMessageDialog(form2, "ERROR: You cannot enter a negative quantity!");
+
+					}
+					// Change quantity if valid quantity & write to the customer file
 					else if (res <= ItemCatalog.getItemSpecification(wc.getValueAt(modelRow, 4).toString())
 							.getQuantity()) {
 						wc.setValueAt(res, modelRow, 2);
-						
+
 						try {
+							// Initialize the Buffered Reader Output
 							BufferedWriter out = new BufferedWriter(new FileWriter(wc.getCustomerFile()));
-							//System.out.println(wc.getCart().toString());
+
+							// Write the cart to the out
 							out.write(wc.getCart().toString());
 							out.close();
-							
 						} catch (IOException e1) {
-							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
-						
-					}
-					//Otherwise quantity is too large for order, ERROR
-					else {
+
+					} else {
+						// Otherwise quantity is too large for order, ERROR
 						JOptionPane.showMessageDialog(form2, "ERROR: Quantity entered exceeded maximum availiable!");
 					}
-					//Fire changes (safety)
+
+					// Fire changes (safety)
 					revalidate();
 				}
 			}
 		};
-		
-		//Creation of edit button column (for each item)
+
+		// Column creation of edit button column (for each item)
 		ButtonColumn editButton = new ButtonColumn(table, edit, 3);
-		
-		//Fire changes
+
+		// Fire changes
 		revalidate();
 	}
 
 	public static void createAndShowGUI(Customer c) {
-		//Implementation of the Shopping cart interface after checkout
+
+		// Implementation of the Shopping cart interface after checkout
+		// Initializing the frame for wish list
 		JFrame frame = new JFrame("Your Wishlist");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
 
+		// Initialize the wish list view (new)
 		WishlistView newContentPane = new WishlistView(c);
-
 		newContentPane.setOpaque(true);
 
+		// Set frame settings for display
 		frame.setContentPane(newContentPane);
 		frame.pack();
 		frame.setVisible(true);
