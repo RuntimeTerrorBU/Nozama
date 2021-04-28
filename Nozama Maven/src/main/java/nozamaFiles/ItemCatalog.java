53c81a172712c3de22b2f92c8835072a0aef2794
Ashley <ashleyreagan613@gmail.com>
2021-04-27 23:03:19 -0500

    ItemCatalog Java Doc completed

--- Nozama Maven/src/main/java/nozamaFiles/ItemCatalog.java
+++ Nozama Maven/src/main/java/nozamaFiles/ItemCatalog.java
@@ -8,26 +8,59 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * The ItemCatalog class creates an the catalog of an item
+ *
+ * @author - Ashley Bickham, Joshua Hunter, Austin Lehman, Tyler Ross
+ * @version 1.0 (Apr 27, 2021)
+ */
 public class ItemCatalog {
 	private static List<ItemSpecification> items = new ArrayList<ItemSpecification>();
 
+	/**
+	 * ItemCatalog constructor
+	 * 
+	 * @param List of ItemSpecification representing the catalog of items
+	 */
 	public ItemCatalog(List<ItemSpecification> items) {
 		ItemCatalog.items = items;
 	}
 
+	/**
+	 * Get the number of items in the Nozama store
+	 * 
+	 * @return static integer representing the number of items
+	 */
 	public static Integer getNumberOfItems() {
 		return items.size();
 	}
 
+	/**
+	 * Get the data of items in the Nozama store
+	 * 
+	 * @return List representing the items in the Nozama Store
+	 */
 	public static List<ItemSpecification> getItems() {
 		return items;
 	}
 
+	/**
+	 * Set the data of items in the Nozama store
+	 * 
+	 * @param List representing the items to be set in the Nozama Store
+	 * @return void
+	 */
 	public static void setItems(List<ItemSpecification> items) {
 		Collections.sort(items, ItemSpecification.compareByID);
 		ItemCatalog.items = items;
 	}
-
+	
+	/**
+	 * Set the item specification of item wanted in the Nozama store
+	 * 
+	 * @param String representing the id of the item to be set
+	 * @return ItemSpecification of the item set
+	 */
 	public static ItemSpecification getItemSpecification(String itemID) {
 		int ndx = Collections.binarySearch(ItemCatalog.items, new ItemSpecification(null, null, null, itemID, null),
 				ItemSpecification.compareByID);
@@ -38,6 +71,12 @@ public class ItemCatalog {
 		return null;
 	}
 
+	/**
+	 * Load the data into the Item Catalog
+	 * 
+	 * @param File to load the data into an item catalog
+	 * @throws IOException if he file is not able to be used
+	 */
 	public static void loadData(File file) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader(file));
 		String line;
