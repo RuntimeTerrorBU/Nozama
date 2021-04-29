f4ffc4e947e80e2bdd4dde8bc4deb39fac732a3e
Alehman74 <Alehman74@yahoo.com>
2021-04-28 21:16:01 -0500

    created jar and updated junit tests

--- Nozama Maven/src/test/java/tests/TestPair.java
+++ Nozama Maven/src/test/java/tests/TestPair.java
@@ -1,28 +1,11 @@
 package tests;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-import java.util.stream.Stream;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import nozamaFiles.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import javax.xml.bind.JAXBException;
 public class TestPair {
 	
 	@Test
